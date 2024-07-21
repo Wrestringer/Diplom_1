af592c33df70dada0eb775d10ddffa061dd45618
Danil Martyushev <martyusheff.danil@yandex.ru>
2024-07-21 16:07:36 +0300

    added constants

---
+++ src/main/java/praktikum/constants/Names.java
@@ -0,0 +1,7 @@
+package praktikum.constants;
+
+public class Names {
+    public static final String NAME_OF_BUN = "Имя булочки1";
+    public static final String NAME_OF_INGREDIENT1 = "Имя ингридиента1";
+    public static final String NAME_OF_INGREDIENT2 = "Имя ингридиента2";
+}
